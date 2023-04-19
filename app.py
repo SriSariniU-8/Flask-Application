7df07f3d42077ab80e532196663e077b18ed0769
SriSariniU-8 <SriSariniU@bigthinkcode.com>
2023-04-19 11:50:12 +0530

    admin added

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from wtforms.validators import InputRequired, Length, ValidationError
 import psycopg2
 import re
 from flask_bcrypt import Bcrypt
+from datetime import timedelta
 
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -27,6 +28,21 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    manager_id = db.Column(db.Integer)
+class admin(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    admin_name = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class manager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    manager_name = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+with app.app_context():
+    # db.init_app(app)
+    db.create_all()
+
 
 @app.route('/')
 def home():
@@ -40,68 +56,208 @@ def login():
     if not username or not password:
         return jsonify({"message": "enter username and password"}), 401
 
-    user = User.query.filter_by(username=username).first()
+    user = (User.query.filter_by(username=username).first() or admin.query.filter_by(admin_name=username).first() or manager.query.filter_by(manager_name=username).first())
 
     if not user or not bcrypt.check_password_hash(user.password, password):
         return jsonify({"message": "Invalid username or password"}), 401
 
-    access_token = create_access_token(identity=user.id)
+    access_token = create_access_token(identity=user.id,expires_delta=timedelta(hours=1))
     return jsonify(access_token=access_token), 200
+    return jsonify({"message":"LOgin successful"})
+    # admin.get(email)
 
-@app.route('/display/<int:user_id>', methods=['GET'])
-def display(user_id):
-    user = User.query.filter_by(id = user_id).first()
-    return jsonify({"username": user.username, "password": user.password})
+@app.route('/display', methods=['GET'])
+def display():
+    user_id= request.json.get('id', None)
+    role= request.json.get('role', None)
+    if(role == 'user'):
+        user = User.query.filter_by(id = user_id).first()
+        return jsonify({"username": user.username, "password": user.password})
+
+    if(role == 'admin'):
+        Admin = admin.query.filter_by(id = user_id).first()
+        return jsonify({"username": Admin.admin_name, "password": Admin.password})
+
+    if(role == 'manager'):
+        Manager = manager.query.filter_by(id = user_id).first()
+        return jsonify({"username": Manager.manager_name, "password": Manager.password})
+
+# @app.route('/register', methods=['POST'])
+# def register():
+#     username = request.json.get('username', None)
+#     password = request.json.get('password', None)
+#     role= request.json.get('role', None)
+
+#     if len(username) > 10:
+#         return jsonify({"message": "username too long"}), 400
+
+#     password_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+#     if not re.match(password_regex, password):
+#         return jsonify({"message": "Password should be minimum eight characters, at least one uppercase letter, one lowercase letter and one number"}), 400
+
+#     existing_username = (User.query.filter_by(username=username).first() or admin.query.filter_by(admin_name=username).first() or manager.query.filter_by(manager_name=username).first())
+
+#     if existing_username:
+#         return jsonify({"message": "Already exists"}), 400
+
+#     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+#     if role== 'admin':
+#         new_user= admin(admin_name= username, password= hashed_password)
+
+#     if role== 'user':
+#         new_user = User(username=username, password=hashed_password)
+    
+#     if role== 'manager':
+#         new_user = manager(manager_name=username, password=hashed_password)
+
+#     db.session.add(new_user)
+#     db.session.commit()
+
+#     return jsonify({"message": "register successful"}), 201
 
 @app.route('/register', methods=['POST'])
-def register():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-
-    if len(username) > 10:
-        return jsonify({"message": "username too long"}), 400
-
-    password_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
-    if not re.match(password_regex, password):
-        return jsonify({"message": "Password should be minimum eight characters, at least one uppercase letter, one lowercase letter and one number"}), 400
-
-    existing_username = User.query.filter_by(username=username).first()
-
-    if existing_username:
-        return jsonify({"message": "Already exists"}), 400
-
-    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-    new_user = User(username=username, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": "register successful"}), 201
-
-@app.route('/users/<int:user_id>', methods=['PATCH', 'DELETE'])
 @jwt_required()
-def update_user(user_id):
-    user = User.query.get(user_id)
-
-    if not user:
-        return jsonify({"message": "User not found"}), 404
-
-    if request.method == 'PATCH':
+def register():
+    current_user_id = get_jwt_identity()
+    adminx = admin.query.filter_by(id=current_user_id).first()
+    if adminx:
         username = request.json.get('username', None)
         password = request.json.get('password', None)
+        role= request.json.get('role', None)
 
-        if username:
-            user.username = username
+        if len(username) > 10:
+            return jsonify({"message": "username too long"}), 400
 
-        if password:
-            hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-            user.password = hashed_password
+        password_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+        if not re.match(password_regex, password):
+            return jsonify({"message": "Password should be minimum eight characters, at least one uppercase letter, one lowercase letter and one number"}), 400
 
+        existing_username = (User.query.filter_by(username=username).first() or admin.query.filter_by(admin_name=username).first() or manager.query.filter_by(manager_name=username).first())
+
+        if existing_username:
+            return jsonify({"message": "Already exists"}), 400
+
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+        if role== 'admin':
+            new_user= admin(admin_name= username, password= hashed_password)
+
+        if role== 'user':
+            new_user = User(username=username, password=hashed_password)
+        
+        if role== 'manager':
+            new_user = manager(manager_name=username, password=hashed_password)
+
+        db.session.add(new_user)
         db.session.commit()
-        return jsonify({"message": "User updated successfully"}), 200
 
-    elif request.method == 'DELETE':
-        db.session.delete(user)
-        db.session.commit()
+        return jsonify({"message": "register successful"}), 201
+    
+    else:
+        return jsonify({'msg': 'You do not have permission to access this route'}), 403
+
+
+
+@app.route('/admin-only/<int:user_id>', methods=['PATCH', 'DELETE'])
+@jwt_required()
+def admin_only(user_id):
+    current_user_id = get_jwt_identity()
+    # adminx = admin.get(current_user)
+    adminx = admin.query.filter_by(id=current_user_id).first()
+    if adminx:
+        user = User.query.get(user_id)
+
+        if not user:
+            return jsonify({"message": "User not found"}), 404
+
+        if request.method == 'PATCH':
+            username = request.json.get('username', None)
+            password = request.json.get('password', None)
+
+            if username:
+                user.username = username
+
+            if password:
+                hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+                user.password = hashed_password
+
+            db.session.commit()
+            return jsonify({"message": "User updated successfully"}), 200
+
+        elif request.method == 'DELETE':
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify({"message": "deleted successfully"})
+
+    else:
+        return jsonify({'msg': 'You do not have permission to access this route'}), 403
+
+@app.route('/adminaccess-only', methods=['PATCH'])
+@jwt_required()
+def adminaccess_only():
+    current_user_id = get_jwt_identity()
+    # adminx = admin.get(current_user)
+    adminx = admin.query.filter_by(id=current_user_id).first()
+    if adminx:
+        user_id= request.json.get("emp_id", None)
+        manager_id= request.json.get("mentor_id", None)
+    
+        # user = User.query.get(user_id)
+        # managerr = manager.query.get(manager_id)
+        user = User.query.filter_by(id=user_id).first()
+        managerr= manager.query.filter_by(id=manager_id).first()
+
+        if (not user or not managerr):
+            return jsonify({"message": "User or manager not found"}), 404
+    
+        if managerr:
+            user.manager_id= manager_id
+            db.session.commit()
+            return jsonify({"message": "User updated successfully"}), 200
+        
+        else :
+            return jsonify({"message": "enter valid manager_id"})
+
+    else:
+        return jsonify({'msg': 'You do not have permission to access this route'}), 403
+
+@app.route('/admin_change_role', methods=['PATCH'])
+@jwt_required()
+def admin_change_role():
+    current_user_id = get_jwt_identity()
+    # adminx = admin.get(current_user)
+    adminx = admin.query.filter_by(id=current_user_id).first()
+    if adminx:
+        user_id= request.json.get("user_id", None)
+        current_role= request.json.get("current_role", None)
+        role_change= request.json.get("role_change", None)
+
+    
+        # user = User.query.get(user_id)
+        # managerr = manager.query.get(manager_id)
+        if(current_role== 'employee'):
+            user = User.query.filter_by(id=user_id).first()
+            if user is not None:
+                user_copy = User(username=user.username, password=user.password)
+                db.session.delete(user)
+                if role_change== 'admin':
+                    changed_user = admin(admin_name = user_copy.username , password= user_copy.password)
+                    db.session.add(changed_user)
+                    db.session.commit()
+                    return jsonify({"message": "role changed to admin"})
+                
+                else:
+                    changed_user = manager(manager_name = user_copy.username , password= user_copy.password)
+                    db.session.add(changed_user)
+                    db.session.commit()
+                    return jsonify({"message": "role changed to manager"})
+                
+                        
+        return jsonify({"message": "admin-rights"})
+        
+    else:
+        return jsonify({'msg': 'You do not have permission to access this route'}), 403
+
+
 
 
 @app.route('/protected' ,methods = ['GET'])
